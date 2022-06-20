c0983f2f038f69221a04f2ab3e4ab793187cb8cf
‘cpkang’ <442090480@qq.com>
2022-06-20 15:41:50 +0800

    feat: 发布到 github.io

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,47 @@
+const moment = require('moment');
+
+moment.locale("zh-cn");
+
+module.exports = {
+    base:"/demo/",
+    title:"好心情",
+    description:"博客的描述",
+    plugins: [
+        [
+          '@vuepress/last-updated',
+          {
+            transformer: (timestamp) => {
+              return moment(timestamp).format("LLLL")
+            }
+          }
+        ]
+      ],
+    themeConfig: { 
+      lastUpdated: '更新时间',
+      logo: '/assets/img/logo.png',
+    //   navbar: false,
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'Guide', link: '/about' },
+        {
+            text: 'Languages',
+            items: [
+              { text: 'Group1', items: [
+                { text: 'Home', link: '/' },
+                { text: 'Guide', link: '/about' },
+              ] },
+              { text: 'Group2', items: [
+                { text: 'Home', link: '/' },
+                { text: 'Guide', link: '/about' },
+              ] }
+            ]
+          },
+        { text: 'External', link: 'https://google.com' },
+      ],
+      sidebar: [
+        '',
+        'about',
+        'about1'
+      ]
+    }
+  }
